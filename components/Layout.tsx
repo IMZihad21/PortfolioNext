35d3e6b2025420d4c9e6992c05bc2b6c1339389f
ZèD <imzihad@gmail.com>
2021-12-10 16:06:58 +0600

    Wrap a layout component around app
    Signed-off-by: ZèD <imzihad@gmail.com>

---
+++ components/Layout.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Layout: NextPage = ({ children }) => {
+    return (
+        <>
+            <main>{children}</main>
+        </>
+    )
+}
+
+export default Layout
