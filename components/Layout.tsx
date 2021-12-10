815fad2ae7dfbe7583771edc936885e7ec189b3b
ZèD <imzihad@gmail.com>
2021-12-10 17:55:58 +0600

    Added banner with important links
    Signed-off-by: ZèD <imzihad@gmail.com>

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -4,11 +4,11 @@ import Footer from './Footer'
 
 const Layout: NextPage = ({ children }) => {
     return (
-        <>
+        <div className="pt-5 md:pt-10 px-5 pb-5 md:px-40 min-h-screen">
             <Navbar />
             <main className='mb-6'>{children}</main>
             <Footer />
-        </>
+        </div>
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
+import Banner from '../components/Banner'
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <h1 className="text-3xl text-center">
-        Hello world!
-      </h1>
+    <div className='px-10'>
+      <Head>
+        <title>Mofajjal Rasul | Full-Stack Web Developer</title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossOrigin="anonymous"></link>
+      </Head>
+      <Banner />
     </div>
   )
 }
