620424237e042d91921538fe598c263eabfbffc7
ZèD <imzihad@gmail.com>
2021-12-11 00:46:37 +0600

    Added project detail page
    Signed-off-by: ZèD <imzihad@gmail.com>

--- components/ProjectLists.tsx
+++ components/ProjectLists.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Image from 'next/image';
 import Link from 'next/link';
 import Project from '../interfaces/Project'
 
@@ -15,11 +16,11 @@ const ProjectLists: NextPage<ProjectLists> = ({ projects }) => {
                     {
                         projects?.map(project =>
                             <div key={project.id} className="relative h-[580px] rounded-lg shadow-lg">
-                                <img className="w-full h-full rounded-lg" src={project.cover} alt={project.name} />
+                                <Image className='rounded-lg' layout='fill' src={project.cover} alt={project.name} />
 
                                 <div className="absolute bottom-0 text-center rounded-lg bg-black dark:bg-white text-white dark:text-black bg-opacity-75 dark:bg-opacity-75 w-full py-4">
                                     <h4 className="mb-3 text-2xl font-semibold tracking-tight">{project.name}</h4>
-                                    <Link href={`/project/${project.id}`}>
+                                    <Link href={`/project/${project.id}`} passHref>
                                         <button className="bg-white dark:bg-black text-xl text-black dark:text-white px-20 py-2 rounded-lg">More Details</button>
                                     </Link>
                                 </div>
--- pages/project/[id].tsx
+++ pages/project/[id].tsx
@@ -1,4 +1,5 @@
 import type { GetStaticPaths, NextPage } from 'next'
+import Image from 'next/image'
 import { server } from '../../config'
 import Project from '../../interfaces/Project'
 
@@ -9,9 +10,30 @@ interface ProjectType {
 const Project: NextPage<ProjectType> = ({ project }) => {
     return (
         <div>
-            <h1 className="text-3xl text-center">
-                {project.name}
-            </h1>
+            <h1 className="text-4xl md:text-5xl font-bold leading-normal">{project?.name}</h1>
+            <p className="mt-5 text-xl md:text-2xl">{project?.description}</p>
+            <div className="py-10 space-x-1 md:space-x-5">
+                {
+                    project?.links?.map(btn => <a
+                        className="bg-black text-white rounded-lg py-3 px-2 md:px-10"
+                        href={btn.link}
+                        target="_blank"
+                        rel="noreferrer"
+                        key={btn.name}
+                    >
+                        {btn.name}
+                    </a>)
+                }
+            </div>
+            <h1 className="text-2xl md:text-3xl mt-5 font-bold leading-normal">Screnshots</h1>
+            <div className="mt-10 grid md:grid-cols-2 gap-5">
+                {/* Replace with carousel */}
+                {
+                    project?.screenshots.map(img => <div key={img} className="bg-black relative rounded-lg p-2 h-96 overflow-y-visible">
+                        <Image layout="fill" objectFit="contain" className="rounded-lg w-full h-auto" src={img} alt={img} />
+                    </div>)
+                }
+            </div>
         </div>
     )
 }
